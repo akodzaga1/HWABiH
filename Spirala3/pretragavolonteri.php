c57d210effcd0f8326a52462a9c885f62572a5ce
Amer Kodzaga <akodzaga1@etf.unsa.ba>
2016-12-30 21:59:34 +0100

    Dodani testni podaci

--- Spirala3/pretragavolonteri.php
+++ Spirala3/pretragavolonteri.php
@@ -16,7 +16,7 @@ if (strlen($q)>0) {
     $ime=$x->item($i)->getElementsByTagName('ime');
     $datum_rodjenja=$x->item($i)->getElementsByTagName('datum_rodjenja');
     $adresa=$x->item($i)->getElementsByTagName('adresa');
-    if ($prezime->item(0)->nodeType==1) {
+    if ($prezime->item(0)->nodeType==1 && $prezime->item(0)->nodeType==1) {
       if (stristr($prezime->item(0)->childNodes->item(0)->nodeValue,$q) || stristr($ime->item(0)->childNodes->item(0)->nodeValue,$q)) {
         if ($hint=="") {
           $hint = '<div class="kolona sest" id="karton">' . "<form method='post' action='prikazvolontera.php?obrisi=1'>" . '<input type="hidden" name="obrisiIndeks" id="obrisiIndeks" value="' .  $i . '"><img src="images/' . $foto->item(0)->childNodes->item(0)->nodeValue . '"' . ' alt="volonter_slika">Prezime: ' . $prezime->item(0)->childNodes->item(0)->nodeValue . '<br>Ime: ' . $ime->item(0)->childNodes->item(0)->nodeValue . '<br>Datum rođenja: ' . $datum_rodjenja->item(0)->childNodes->item(0)->nodeValue . '<br>Adresa: ' . $adresa->item(0)->childNodes->item(0)->nodeValue . '<br>Indeks: (' . $i . ')' . '<br><button type="submit" name="x">x</button>' . '</form>' . "<form method='post' action='prikazvolontera.php?edit=1'>" . '<input type="hidden" name="izmijeniIndeks" id="izmijeniIndeks" value="' . $i . '">' . '<button type="submit" name="izmijeni">Izmijeni</button>' . '</form>' . '</div>';
